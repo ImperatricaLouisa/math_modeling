c2c4b09f714ec822b6b34deaa0e2725fdb635062
ImperatricaLoui <86868898768@mail.ru>
2021-03-14 23:51:03 +0000

    сделала первую лабу

---
+++ lab1_task1.py
@@ -0,0 +1,34 @@
+print('Привет мир ^^ !')
+print("Маша + Петя = Любовь")
+x=3+4
+print(x)
+print('х =',x)
+
+print(type('Hello'))
+print(type(x))
+x=3/4
+print(type(x))
+x=[1.13,13,13/13]
+print(type(x))
+
+x=int(input('Господин, введите значение х, пожалуйста '))
+y=(x**3/2)/(x**3+3/x)*(4*x**7-x**5)+80*(27*x**4+12*x**3-5*x**2+10)**0.5
+print(y)
+y=(3%2+16.7//4.32)/(14.5+31%12-x**3.4//1)
+print(y)
+
+a=[1,13,'ня','тяф']
+print(type(a))
+b=[313,'конь','печенька',131]
+print(type(b))
+
+k=str(a[1])+str(b[3])
+print(k)
+h=str(a[2])+str(b[2])
+print(h)
+p=a[0]*b[0]
+print(p)
+p=a[1]**b[3]
+print(p)
+p=a+b
+print(p)
---
+++ lec_1_tipe.py
@@ -0,0 +1,7 @@
+type #определение типа данных
+print(type(3))
+a='molodec'
+a=['molodec',1,'nia',13]
+print(type(a))
+
+
