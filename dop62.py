0441457ffeb5bbbbb3596d6eaf026c7a8b460d11
ImperatricaLoui <80589179+ImperatricaLouisa@users.noreply.github.com>
2021-07-19 16:03:24 +0000

    fgjs

---
+++ dop62.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+E = np.arange ( 0 , 1 )
+fi = np.linspace(0, 2*np.pi, 100)
+
+rx = 2
+ry = 1
+p = ( ry )**2 / rx 
+
+r = p / ( 1 + E * np.cos(fi))
+
+plt.plot( r )
+plt.axis('equal')
+plt.show()
+    	
